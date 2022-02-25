01f06eb42f06ef5901532f89443ef208da37ad8e
buffalo0o <66760267+buffalo0o@users.noreply.github.com>
2022-02-25 17:26:09 +0800

    Create index.php

---
+++ content/index.php
@@ -0,0 +1,9 @@
+<html>
+    <head>
+        <title>PHP Hello Airasia!</title>
+    </head>
+    <body>
+        <?php echo '<h1>Hello Airasia</h1>'; ?>
+        <?php phpinfo(); ?>
+    </body>
+</html>
