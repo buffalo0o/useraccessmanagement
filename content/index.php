9c5f9c631206292051599a186ff10e6eb45db099
buffalo0o <66760267+buffalo0o@users.noreply.github.com>
2022-02-25 17:31:20 +0800

    Update index.php

--- content/index.php
+++ content/index.php
@@ -3,7 +3,7 @@
         <title>PHP Hello Airasia!</title>
     </head>
     <body>
-        <?php echo '<h1>Hello Airasia</h1>'; ?>
+        <?php echo '<h1>Hello Airasia!! This is from Infrateam </h1>'; ?>
         <?php phpinfo(); ?>
     </body>
 </html>
